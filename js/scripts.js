b72fee066cfb7df5aee4ad3b125688e3355f4595
DavidRoaF <78501367+DavidRoaF@users.noreply.github.com>
2021-03-03 12:06:45 -0700

    Stylize hamburger menu

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,10 @@ console.log("hello world")
 var escapedText1 = "He said: \"Yes!\", that's for sure";
 var escapedText2 = 'He said: "Yes!", that\'s for sure';
 function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+  x.className += ' responsive';
+} else {
+  x.className = 'navtoggle';
+}
 }
