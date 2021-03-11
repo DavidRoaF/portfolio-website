2f9ed5845ad1b388c1cdec95c2496e56af1fef2d
DavidRoaF <78501367+DavidRoaF@users.noreply.github.com>
2021-03-10 19:22:34 -0700

    Fixed css bugs

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,4 @@
-console.log("hello world")
-var escapedText1 = "He said: \"Yes!\", that's for sure";
-var escapedText2 = 'He said: "Yes!", that\'s for sure';
+
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
   if (x.className === 'navtoggle') {
